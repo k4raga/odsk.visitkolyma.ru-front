808bed26d3cdcedf7bfab01c23b791ed9332e51b
k4raga <illkiri02@mail.ru>
2022-12-27 01:16:02 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const mode = process.env.NODE_ENV || 'development';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        hot: true,
+        historyApiFallback: true,
+    },
+    entry: ['@babel/polyfill',path.resolve(__dirname, 'src', 'index.js')],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: 'scripts.js',
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: "ui.html",
+            template: path.resolve(__dirname, 'src/html', 'ui.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.html$/i,
+            loader: "html-loader",
+        },
+            {
+                test: /\.(c|le)ss$/i,
+                use: [
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [require('postcss-preset-env')]
+                            }
+                        }
+                    },
+                    'less-loader'],
+            },
+            {
+                test: /\.m?(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }
+}
