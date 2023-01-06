e10e66b47a7bf8fb0cfd8f5fcb74d0243c5fe94d
k4raga <illkiri02@mail.ru>
2023-01-06 12:54:22 +0300

    немного адаптива

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './css/const.less';
 import './css/global.less'
 import './css/elements.less';
 import './css/style.less';
+import './css/mixin.less';
 import './js/script';
 
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,8 @@
 window.addEventListener('DOMContentLoaded', (e) => {
     const BUTTONS_VOTE = document.querySelectorAll('.btn-vote')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    function closeBtn() {
+    }
 
 
 })
