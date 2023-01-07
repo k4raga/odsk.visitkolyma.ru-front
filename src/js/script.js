c0a0d6ce53b67674a6b7f3c33e9722137de5eb31
k4raga <illkiri02@mail.ru>
2023-01-07 23:25:55 +0300

    тег

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
     const BUTTONS_VOTE = document.querySelectorAll('.btn-vote')
     const POPUP_AUTHORIZATION = document.querySelector('.authorization')
     const CLOSE_BUTTON_AUTHORIZATION = POPUP_AUTHORIZATION.querySelector('.close')
-
+    const NOMINEE_ITEMS = document.querySelector('.nominee-items')
 
     function togglePopup(el) {
         el.classList.toggle('active')
@@ -20,5 +20,24 @@ window.addEventListener('DOMContentLoaded', (e) => {
         togglePopup(POPUP_AUTHORIZATION)
     })
 
+    function createCard(nomineeItemLink, nomineeImg, nomineePersonImg,nomineePersonName) {
+  //       const content = `
+  //       <div class="nominee-item">
+  //           <a class='nominee-item-link' href="/detail/">
+  //               <div class="nominee-photo">
+  //                   <img class="nominee-img" src="../img/galey2.jpg" alt="фото участника">
+  //                   <img class="nominee-search" src="../img/nominee-search.png" alt="подробнее">
+  //               </div>
+  //               <div class="nominee-person">
+  //                   <img class="nominee-person-img" src="../img/nominee-min.png" alt="Фото участника">
+  //                   <div class="nominee-person-name">Андрей Шишокин</div>
+  //               </div>
+  //           </a>
+  //       </div>
+  // `;
 
+
+
+
+    }
 })
