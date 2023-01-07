a2716fce2c88d30e9744c93cad1b5a8e18aeb9cd
k4raga <illkiri02@mail.ru>
2023-01-07 23:46:37 +0300

    тег

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', (e) => {
     const BUTTONS_VOTE = document.querySelectorAll('.btn-vote')
     const POPUP_AUTHORIZATION = document.querySelector('.authorization')
-    const CLOSE_BUTTON_AUTHORIZATION = POPUP_AUTHORIZATION.querySelector('.close')
+    // const CLOSE_BUTTON_AUTHORIZATION = POPUP_AUTHORIZATION.querySelector('.close')
     const NOMINEE_ITEMS = document.querySelector('.nominee-items')
 
     function togglePopup(el) {
@@ -16,28 +16,28 @@ window.addEventListener('DOMContentLoaded', (e) => {
         })
     }
 
-    CLOSE_BUTTON_AUTHORIZATION.addEventListener('click', (e) => {
-        togglePopup(POPUP_AUTHORIZATION)
-    })
+    // CLOSE_BUTTON_AUTHORIZATION.addEventListener('click', (e) => {
+    //     togglePopup(POPUP_AUTHORIZATION)
+    // })
 
     function createCard(nomineeItemLink, nomineeImg, nomineePersonImg,nomineePersonName) {
-  //       const content = `
-  //       <div class="nominee-item">
-  //           <a class='nominee-item-link' href="/detail/">
-  //               <div class="nominee-photo">
-  //                   <img class="nominee-img" src="../img/galey2.jpg" alt="фото участника">
-  //                   <img class="nominee-search" src="../img/nominee-search.png" alt="подробнее">
-  //               </div>
-  //               <div class="nominee-person">
-  //                   <img class="nominee-person-img" src="../img/nominee-min.png" alt="Фото участника">
-  //                   <div class="nominee-person-name">Андрей Шишокин</div>
-  //               </div>
-  //           </a>
-  //       </div>
-  // `;
-
-
-
-
+        const content = `
+        <div class="nominee-item">
+            <a class='nominee-item-link' href="${nomineeItemLink}">
+                <div class="nominee-photo">
+                    <img class="nominee-img" src="${nomineeImg}" alt="фото участника">
+                    <img class="nominee-search" src="../img/nominee-search.png" alt="подробнее">
+                </div>
+                <div class="nominee-person">
+                    <img class="nominee-person-img" src="${nomineePersonImg}" alt="Фото участника">
+                    <div class="nominee-person-name">${nomineePersonName}</div>
+                </div>
+            </a>
+        </div>
+         
+  `;
+        NOMINEE_ITEMS.innerHTML += content
     }
+    createCard('link', 'img', 'img2', 'Иван')
+
 })
