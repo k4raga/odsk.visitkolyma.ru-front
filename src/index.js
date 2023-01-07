372df8731abdd65e1606f1e0c838be479399be27
k4raga <illkiri02@mail.ru>
2023-01-07 23:12:51 +0300

    еще немного адаптива

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './css/const.less';
 import './css/global.less'
 import './css/elements.less';
 import './css/style.less';
+import './css/detail.less';
 import './css/mixin.less';
 import './js/script';
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,24 @@
 window.addEventListener('DOMContentLoaded', (e) => {
     const BUTTONS_VOTE = document.querySelectorAll('.btn-vote')
+    const POPUP_AUTHORIZATION = document.querySelector('.authorization')
+    const CLOSE_BUTTON_AUTHORIZATION = POPUP_AUTHORIZATION.querySelector('.close')
 
-    function closeBtn() {
+
+    function togglePopup(el) {
+        el.classList.toggle('active')
     }
+
+    // показываем авторизацию
+    for (let i = 0; i < BUTTONS_VOTE.length; i++) {
+        let button_vote = BUTTONS_VOTE[i]
+        button_vote.addEventListener('click', (e) => {
+            togglePopup(POPUP_AUTHORIZATION)
+        })
+    }
+
+    CLOSE_BUTTON_AUTHORIZATION.addEventListener('click', (e) => {
+        togglePopup(POPUP_AUTHORIZATION)
+    })
 
 
 })
